50e2fcac01cc1197fc04c3cbc9ea5368f503eecd
Soufiane <sfn.elmansoury@gmail.com>
2024-09-04 01:49:37 +0200

    add toggleSwitch button with animation

--- components/Home.js
+++ components/Home.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect} from 'react';
 import { Popover, Button, Input } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,10 +8,14 @@ import 'antd/dist/antd.css';
 import styles from '../styles/Home.module.css';
 import Image from 'next/image'
 import ImageBack from "../assets/napoleon-2023-movie-joaquin-phoenix-portrait-uhd-4k-wallpaper.jpeg";
+import { motion, spring } from "framer-motion";
 
 function Home() {
   const [likedMovies, setLikedMovies] = useState([]);
   const [moviesData, setMoviesData]=useState([]);
+  const [isOn, setIsOn] =useState(false);
+
+  const toggleSwitch= ()=> setIsOn(!isOn);
 
 
   useEffect(() => {
@@ -59,30 +64,52 @@ function Home() {
     </div>
   );
 
-  // Movies list
-  // const moviesData = [
-  //   { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
-  //   { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
-  //   { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
-  //   { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
-  // ];
-
   const movies = moviesData.map((data, i) => {
     const isLiked = likedMovies.some(movie => movie === data.title);
     return <Movie key={i} updateLikedMovies={updateLikedMovies} isLiked={isLiked} title={data.title} overview={data.overview} poster={data.poster} voteAverage={data.voteAverage} voteCount={data.voteCount} />;
   });
 
+
+  const spring = {
+    type: "spring",
+    stiffness: 500,
+    damping: 20,
+  }
+
+
   return (
     <div className={styles.main}>
       <div className={styles.header}>
-        <div className={styles.logocontainer}>
+        <div className={styles.logoContainer}>
           <img src="logo.png" alt="Logo" />
           <img className={styles.logo} src="logoletter.png" alt="Letter logo" />
+        </div>
+        <div data-isOn={isOn} onClick={toggleSwitch} className={styles.switchContainer}>
+          <motion.div className={styles.handle} layout transition={spring}/>
         </div>
         <Popover title="Liked movies" content={popoverContent} className={styles.popover} trigger="click">
           <Button>♥ {likedMovies.length} movie(s)</Button>
         </Popover>
       </div>
+      {/* <div className={styles.animContainer}>
+        <h1 className={styles.titleDiv}>
+          Show me on scroll
+        </h1>
+        <motion.div
+        animate={{
+          x:0,
+          scale:'1',
+          rotate:'0',
+          zIndex:'1',
+          backgroundColor:'#000',
+          boxShadow:'10px 10px 0 rgba(0,0,0, 0.2)',
+          position:'fixed',
+          transitionEnd: {
+            display:'none',
+          },
+        }}
+        />
+      </div> */}
       <div className={styles.backgroundContainer}>
         <Image
         src={ImageBack}
